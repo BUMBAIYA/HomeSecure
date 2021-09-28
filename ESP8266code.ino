648e5f8e42dcb337349c055dcf8b0960d3c96943
BUMBAIYA <amitchauhan263871@gmail.com>
2021-09-29 00:33:30 +0530

    final edit

--- ESP8266code.ino
+++ ESP8266code.ino
@@ -1,153 +1,86 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <FS.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 #include <Adafruit_Fingerprint.h>
+#include <FS.h>
 #include "index.h"
 
 const unsigned long eventInterval = 1000;
 unsigned long previousTime = 0;
 
+const unsigned long eventInterval2 = 30000;
+unsigned long previousTime2 = 0;
+
+const long utcOffSetInSeconds = 19802;
+
 const char* ssid = "Chauhan";
 const char* password = "9619886170";
-const uint8_t GPIOPIN[4] = {D5,D6,D7,D8};
-String data;
-String id = "NA";
-String fingerStatus = "null";
+const uint8_t gpio[4] = {D5,D6,D7,D8};
+char buff[24];
+String data[4];
+int pos;
+int id;
+String state = "OFF";
+String dataCheck = "OK";
+int pin;
+
 ESP8266WebServer server(80);
 SoftwareSerial mySerial(D1,D2);
-Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial); 
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "asia.pool.ntp.org",utcOffSetInSeconds);
 
-void handleRoot() 
-{
- String s = webpage;
- server.send(200, "text/html", s);
-}
-
-void led_control() 
-{
-  String state = "OFF";
-  String act_state = server.arg("state");
-  float led_float = act_state.toFloat();
-  if (led_float != 0) {
-    int pinState = led_float;
-    int btnNumber = ((led_float*10) - (pinState*10));
-    Serial.println(pinState);
-    Serial.println(btnNumber);
-    if (pinState == 1) {
-      if (btnNumber == 5) {
-        digitalWrite(D5,HIGH);
-        Serial.println("led5 ON");
-      }
-      else if (btnNumber == 6) {
-        digitalWrite(D6,HIGH);
-        Serial.println("led6 ON");
-      }
-      else if (btnNumber == 7) {
-        digitalWrite(D7,HIGH);
-        Serial.println("led7 ON");
-      }
-      else if (btnNumber == 8) {
-        digitalWrite(D8,HIGH);
-        Serial.println("led8 ON");
-      }
-      else {
-        Serial.println("Error at active pin but no btn number");
-      }
-      state="ON";
-      server.send(200, "text/plain",state);
-      return;
-     }
-     else if (pinState == 0) {
-          if (btnNumber == 5) {
-            digitalWrite(D5,LOW);
-            Serial.println("led5 OFF");
-          }
-          else if (btnNumber == 6) {
-            digitalWrite(D6,LOW);
-            Serial.println("led6 OFF");
-          }
-          else if (btnNumber == 7) {
-            digitalWrite(D7,LOW);
-            Serial.println("led7 OFF");
-          }
-          else if (btnNumber == 8) {
-            digitalWrite(D8,LOW);
-            Serial.println("led8 OFF");
-          }
-          else {
-            Serial.println("Error at active pin but no btn number");
-          }
-          state="OFF";
-          server.send(200, "text/plain",state);
-          return;
-      }
-      else {
-        Serial.println("Error is PinState in not defined");
-        return;
-      }
-  }
-  else {
-    Serial.println("Error converting string to float");
-    return;
-  }
-}
-
-void setup(void)
-{
-  for(int x=0;x<4;x++) { 
-    pinMode(GPIOPIN[x],OUTPUT);
-  }
+void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  Serial.println("");
-  
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print("Connecting...");
+  for(int x=0;x<4;x++) {
+    pinMode(gpio[x], OUTPUT);
   }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
 
   finger.begin(57600);
-  if (finger.verifyPassword()) {
-    Serial.println("Fingerprint sensor found");
+
+  WiFi.begin(ssid,password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
   }
-  else {
-    Serial.println("Did not find fingerprint sensor");
-  }
-  Serial.println(F("Reading sensor parameters"));
-  finger.getParameters();
-  Serial.print(F("Status: 0x")); Serial.println(finger.status_reg, HEX);
-  Serial.print(F("Sys ID: 0x")); Serial.println(finger.system_id, HEX);
-  Serial.print(F("Capacity: ")); Serial.println(finger.capacity);
-  Serial.print(F("Security level: ")); Serial.println(finger.security_level);
-  Serial.print(F("Device address: ")); Serial.println(finger.device_addr, HEX);
-  Serial.print(F("Packet len: ")); Serial.println(finger.packet_len);
-  Serial.print(F("Baud rate: ")); Serial.println(finger.baud_rate);
- 
-  server.on("/", handleRoot);
+
+  Serial.println("Connected to selected wifi");
+  Serial.print("IP Address: "); Serial.println(WiFi.localIP());
+
+  server.on("/", sendWebPage);
   server.on("/bootstrap.min.css", bootstrap);
   server.on("bootstrap.min.css", bootstrap);
-  server.on("/bootstrap.min.js", bootstrapmin);
-  server.on("bootstrap.min.js", bootstrapmin);
-  server.on("/led_set", led_control);
-  server.on("/sendData", sendSensorData);
+  server.on("/bootstrap.min.js", jsbootstrap);
+  server.on("bootstrap.min.js", jsbootstrap);
+  server.on("/led_control", ledControl);
+  server.on("/recData", sendSensorData);
+  server.on("/schData", saveData);
+  server.on("/sds", selectedDelete);
+  server.on("/disPill", pillState);
   server.begin();
   SPIFFS.begin();
+  timeClient.begin();
 }
 
-void loop(void)
-{
+void loop() {
   unsigned long currentTime = millis();
   if (currentTime - previousTime >= eventInterval) {
-    getFingerprintID();
+    id = getID();
     previousTime = currentTime;
   }
+  unsigned long currentTime2 = millis();
+  if (currentTime2 - previousTime2 >= eventInterval2) {
+    checkScheduleRun();
+    previousTime2 = currentTime2;
+  }
   server.handleClient();
+}
+
+void sendWebPage() {
+  String c = WebPage;
+  server.send(200, "text/html", c);
 }
 
 void bootstrap() {
@@ -155,87 +88,133 @@ void bootstrap() {
   size_t sent = server.streamFile(file, "text/css");
 }
 
-void bootstrapmin() {
+void jsbootstrap() {
   File file = SPIFFS.open("/bootstrap.min.js.gz", "r");
   size_t sent = server.streamFile(file, "application/javascript");
 }
 
-uint8_t getFingerprintID() {
-  uint8_t p = finger.getImage();
-  switch (p) {
-    case FINGERPRINT_OK:
-      Serial.println("Image taken");
-      break;
-    case FINGERPRINT_NOFINGER:
-      Serial.println("No finger detected");
-      fingerStatus = "null";
-      id = "NA";
-      return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      return p;
-    case FINGERPRINT_IMAGEFAIL:
-      Serial.println("Imaging error");
-      return p;
-    default:
-      Serial.println("Unknown error");
-      return p;
+void ledControl() {
+  String s = server.arg("ledData");
+  float randomFloat = s.toFloat();
+  int led_state = randomFloat;
+  int led_pin = ((randomFloat*10) - (led_state*10));
+  if (led_state == 1) {
+    digitalWrite(gpio[led_pin],HIGH);
+    pin = led_pin;
+    state = "ON";
+    server.send(200, "text/plain","ON");
   }
-
-  // OK success!
-
-  p = finger.image2Tz();
-  switch (p) {
-    case FINGERPRINT_OK:
-      Serial.println("Image converted");
-      break;
-    case FINGERPRINT_IMAGEMESS:
-      Serial.println("Image too messy");
-      return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      return p;
-    case FINGERPRINT_FEATUREFAIL:
-      Serial.println("Could not find fingerprint features");
-      return p;
-    case FINGERPRINT_INVALIDIMAGE:
-      Serial.println("Could not find fingerprint features");
-      return p;
-    default:
-      Serial.println("Unknown error");
-      return p;
+  else {
+    digitalWrite(gpio[led_pin],LOW);
+    pin = led_pin;
+    state = "OFF";
+    server.send(200, "text/plain","OFF");
   }
-
-  // OK converted!
-  p = finger.fingerSearch();
-  if (p == FINGERPRINT_OK) {
-    Serial.println("Found a print match!");
-  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
-    Serial.println("Communication error");
-    return p;
-  } else if (p == FINGERPRINT_NOTFOUND) {
-    Serial.println("Did not find a match");
-    // here the data edit code will be injected for esp8266 WebServer
-    Serial.println("Fingerprint did not matched");
-    fingerStatus = "Failed";
-    id = "NA";
-    return p;
-  } else {
-    Serial.println("Unknown error");
-    return p;
-  }
-  // found a match!
-  Serial.print("Found ID #"); Serial.print(finger.fingerID);
-  Serial.print(" with confidence of "); Serial.println(finger.confidence);
-  // here the entry log code will be injected for the esp8266 WebServer
-  Serial.println("Fingerprint matched Successfully");
-  int x = finger.fingerID;
-  id = String(x);
-  fingerStatus = "Passed";
-  return finger.fingerID;
 }
 
 void sendSensorData() {
-  data = fingerStatus + id;
-  server.send(200, "text/plain",data);
+  server.send(200, "text/plain", String(id));
+}
+
+int getID() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK)  return -1;
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  return -2;
+  return finger.fingerID;
+}
+
+void saveData() {
+  dataCheck = "OK";
+  String data = server.arg("data");
+  Serial.println(data);
+  File file = SPIFFS.open("/scheduleLog.txt", "a+");
+  file.print(data);
+  file.close();
+  Serial.println("Data written in the file");
+  readStoreData();
+}
+
+void readStoreData() {
+  File file = SPIFFS.open("/scheduleLog.txt", "r");
+  Serial.print("FILE CONTENT: ");
+  while (file.available()) {
+    char c = file.read();
+    buff[pos] = c;
+    pos++;
+  }
+  Serial.println(buff);
+  String c = String(buff);
+  Serial.println("Reading file is done.");
+  pos = 0;
+  for (int a=0;a<4;a++) {
+    int i = (a * 6);
+    int y = ((a+1) * 6);
+    String t = c.substring(i,y);
+    data[a] = t;
+    Serial.println(data[a]);
+  }
+  for (int x=0;x<4;x++) {
+    if (data[x].length() < 6 && data[x].length() > 0){
+      dataCheck = "ER";
+    }
+  }
+  server.send(200, "text/plain", dataCheck);
+}
+
+void selectedDelete() {
+  String ind = server.arg("index");
+  int index = ind.toInt();
+  data[index] = "";
+  memset(buff,0,sizeof buff);
+  SPIFFS.remove("/scheduleLog.txt");
+  File file = SPIFFS.open("/scheduleLog.txt", "a+");
+  for(int x=0;x<4;x++) {
+    if (data[x].length() >5) {
+      Serial.println(data[x]);
+      file.print(data[x]);
+    }
+  }
+  file.close();
+  Serial.println("After selected delete");
+  server.send(200, "text/plain", "SDS");
+  readStoreData();
+}
+
+void checkScheduleRun() {
+  timeClient.update();
+  String hr = String(timeClient.getHours());
+  hr = hr.length() == 1 ? "0" + hr : hr;
+  String mn = String(timeClient.getMinutes());
+  mn = mn.length() == 1 ? "0" + mn : mn;
+  String tm = String(hr + ":" + mn);
+  for (int x=0;x<4;x++) {
+    if (data[x].length() == 6) {
+      String h = data[x].substring(2,4);
+      String m = data[x].substring(4,6);
+      String p = data[x].substring(0,1);
+      String mde = data[x].substring(1,2);
+      int md = mde.toInt();
+      int pn = p.toInt();
+      if (tm == h + ":" + m) {
+        if (md == 1) {
+          digitalWrite(gpio[pn],HIGH);
+          state = "ON";
+          pin = pn;
+        }
+        else {
+          digitalWrite(gpio[pn], LOW);
+          state = "OFF";
+          pin = pn;
+        }
+      }
+    }
+  }
+}
+
+void pillState() {
+  String s = String(pin) + state;
+  server.send(200, "text/plain", s);
 }
